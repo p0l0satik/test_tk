93362a20eb42b501a10e58b5cf67a7a1c9ccfb4d
p0l0satik <denis.cucu@yandex.ru>
2020-06-06 15:07:58 +0300

    scroll bar + save as

--- main.py
+++ main.py
@@ -1,28 +1,47 @@
 from tkinter import filedialog
 from tkinter import *
 from tkinter import ttk
+from tkinter import messagebox
+import tkinter.scrolledtext as ScrolledText
 import subprocess
 
 def get_name(*args):
     file_name_v.set(filedialog.askopenfilename(initialdir="/", title="Select file"))
 
 def open_f(*args):
-    proc = subprocess.run(["xxd", file_name_v.get()], stdout=subprocess.PIPE)
-    line = proc.stdout.decode("utf-8")
+    proc = subprocess.run(["xxd", file_name_v.get()], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    if proc.stderr:
+        messagebox.showerror(title="Error", message=proc.stderr.decode("utf-8"))
+        return    
+    line = proc.stdout
     editor.insert(END, line)
     
 def save_f(*args):
     f = open("test", "w+")
     f.write(editor.get("@0,0", END))
     f.close()
-    proc = subprocess.run(["xxd", "-r", "test"], stdout=subprocess.PIPE)
+    proc = subprocess.run(["xxd", "-r", "test"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    if proc.stderr:
+        messagebox.showerror(title="Error", message=proc.stderr.decode("utf-8"))
+        return
     line = proc.stdout.decode("utf-8")
     f = open(file_name_v.get(), "w")
     f.write(line)
     f.close()
 
 def save_as(*args):
-    root.filename = filedialog.asksaveasfilename(initialdir = "/", title = "Save file")
+    filename = filedialog.asksaveasfilename(initialdir = "/", title = "Save file")
+    f = open("test", "w+")
+    f.write(editor.get("@0,0", END))
+    f.close()
+    proc = subprocess.run(["xxd", "-r", "test"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    if proc.stderr:
+        messagebox.showerror(title="Error", message=proc.stderr.decode("utf-8"))
+        return
+    line = proc.stdout.decode("utf-8")
+    f = open(filename, "w")
+    f.write(line)
+    f.close()
 
 root = Tk()
 root.geometry("500x500")
@@ -38,19 +57,20 @@ mainframe.rowconfigure(0, weight=1)
 mainframe.rowconfigure(1, weight=2)
 mainframe.rowconfigure(2, weight=1)
 mainframe.rowconfigure(3, weight=1)
+mainframe.rowconfigure(4, weight=1)
 
 file_name_v = StringVar()
 file_n = ttk.Entry(textvariable=file_name_v)
 file_n.grid(column=0, row=0, sticky="nw")
 
 ttk.Button(mainframe, text="open", command=open_f).grid(column=0, row=1)
-ttk.Button(mainframe, text="save", command=save_f).grid(column=0, row=3, sticky="sw")
-ttk.Button(mainframe, text="save as", command=save_f).grid(column=1, row=3, sticky="sw")
+ttk.Button(mainframe, text="save", command=save_f).grid(column=0, row=2, sticky="s")
+ttk.Button(mainframe, text="save as", command=save_as).grid(column=1, row=2, sticky="s")
 
-ttk.Label(mainframe, text="openfile").grid(column=1, row=0, sticky="nw")
+ttk.Label(mainframe, text="openfile").grid(column=1, row=0, sticky="n")
 
 file_n.bind('<1>', get_name)
 
-editor = Text()
-editor.grid(column=0, row=2)
+editor = ScrolledText.ScrolledText(root)
+editor.grid(column=0, row=3, columnspan=2, sticky="n")
 root.mainloop()
