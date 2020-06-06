ec1770d3b95ea9ea3058aed69386802861ce3b55
p0l0satik <denis.cucu@yandex.ru>
2020-06-06 15:47:24 +0300

    added undo + redo

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ def get_name(*args):
     file_name_v.set(filedialog.askopenfilename(initialdir="/", title="Select file"))
 
 def open_f(*args):
+    current_file.set(file_name_v.get())
+    root.title(current_file.get())
     editor.delete("@0,0", END)
     proc = subprocess.run(["xxd", file_name_v.get()], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     if proc.stderr:
@@ -26,7 +28,7 @@ def save_f(*args):
         messagebox.showerror(title="Error", message=proc.stderr.decode("utf-8"))
         return
     line = proc.stdout.decode("utf-8")
-    f = open(file_name_v.get(), "w")
+    f = open(current_file.get(), "w")
     f.write(line)
     f.close()
 
@@ -43,6 +45,17 @@ def save_as(*args):
     f = open(filename, "w")
     f.write(line)
     f.close()
+
+def undo():
+    try:
+        editor.edit_undo()
+    except:
+        pass
+def redo():
+    try:
+        editor.edit_redo()
+    except:
+        pass
 
 root = Tk()
 root.geometry("500x500")
@@ -61,16 +74,19 @@ mainframe.rowconfigure(3, weight=1)
 mainframe.rowconfigure(4, weight=1)
 
 file_name_v = StringVar()
+current_file = StringVar()
 file_n = ttk.Entry(textvariable=file_name_v)
 file_n.grid(column=0, row=0, sticky="nw")
 
 ttk.Button(mainframe, text="open", command=open_f).grid(column=0, row=1)
 ttk.Label(mainframe, text="openfile").grid(column=1, row=0, sticky="nw")
-editor = ScrolledText.ScrolledText(root)
-editor.grid(column=0, row=3, columnspan=2, sticky="wn")
+# ttk.Label(mainframe, textvariable=current_file).grid(column=1, row=2, columnspan=2, sticky="nw")
+editor = ScrolledText.ScrolledText(root, undo=True)
+editor.grid(column=0, row=2, columnspan=2, sticky="wn")
 ttk.Button(mainframe, text="save", command=save_f).grid(column=0, row=4, sticky="n")
 ttk.Button(mainframe, text="save as", command=save_as).grid(column=1, row=4, sticky="nw")
-
+ttk.Button(mainframe, text="undo", command=undo).grid(column=0, row=5, sticky="n")
+ttk.Button(mainframe, text="redo", command=redo).grid(column=1, row=5, sticky="nw")
 
 file_n.bind('<1>', get_name)
 
